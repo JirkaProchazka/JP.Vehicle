3b72c6b18e965332f4cb647f51ac8dabf19f7b0e
JirkaProchazka <47227216+JirkaProchazka@users.noreply.github.com>
2020-04-01 00:50:21 +0200

    Initial commit

---
+++ JP.Vehicle.MCU/JP.Vehicle.MCU.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		JP.ino
+ Created:	4/1/2020 12:18:52 AM
+ Author:	JP
+*/
+
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
