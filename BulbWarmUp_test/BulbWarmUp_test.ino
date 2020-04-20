bc09086ba17dbe7b3e60f4a1337a8543bff38137
JirkaProchazka <47227216+JirkaProchazka@users.noreply.github.com>
2020-04-20 03:46:12 +0200

    Ovladani Casovace

--- BulbWarmUp_test/BulbWarmUp_test.ino
+++ BulbWarmUp_test/BulbWarmUp_test.ino
@@ -16,6 +16,8 @@ enum PINS
 
 	HEAD_LIGHT_SWITCH_MEASURE,
 	HEAD_LIGHT_SWITCH,
+
+	TEST_SWITCH = 31,
 };
 
 enum ANALOGUE_PINS
@@ -33,11 +35,17 @@ byte HeadlightLevel = 25;
 bool LED_State = false;
 
 int lightLevel = 0;
-int diff = 8;
+int diff = -8;
+
+
+unsigned long ToggleTime = 0;
+bool LastRead = HIGH;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
+	Serial.begin(9600);
+	pinMode(PINS::TEST_SWITCH, INPUT_PULLUP);
 	pinMode(PINS::LED_TEST, OUTPUT);
 
 	noInterrupts();           // disable all interrupts
@@ -48,9 +56,69 @@ void setup() {
 	OCR1A = 624;            // compare match register 16MHz/1024/1Hz
 	TCCR1B |= (1 << WGM12);   // CTC mode
 	TCCR1B |= (1 << CS12) | (0 << CS11) | (1 << CS10); // 1024 prescaler 
-	TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+	//TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
 	interrupts();             // enable all interrupts
 }
+
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+	auto actualRead = digitalRead(PINS::TEST_SWITCH);
+	if ((actualRead != LastRead) && ((millis() - ToggleTime) > 150))
+	{
+		LastRead = actualRead;
+		ToggleTime = millis();
+
+		if (actualRead)
+		{
+			Serial.println("Do Job");
+			diff *= -1;
+			if (!TIMSK1)
+			{
+				Serial.println("start timer");
+				TIMSK1 |= (1 << OCIE1A);
+			}
+		}
+	}
+
+
+
+	/*
+	if ((millis() - ToggleTime) >= ToggleInterval)
+	{
+		//noInterrupts();
+
+		if (state)
+		{
+			Serial.println("turn off");
+			TIMSK1 &= (B11111110 << OCIE1A);
+			TCNT1 = 0;
+			analogWrite(PINS::LED_TEST, 0);
+			lightLevel = 0;
+		}
+		else
+		{
+			Serial.println("turn on");
+			TIMSK1 |= (1 << OCIE1A);
+		}
+
+		state = !state;
+		ToggleTime = millis();
+
+		//interrupts();
+	}
+
+	*/
+
+}
+
+void CheckLight()
+{
+
+}
+
+
 
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
@@ -60,22 +128,28 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 	lightLevel += diff;
 	lightLevel = constrain(lightLevel, 0, 255);
 
-	if(lightLevel < 255)
-	analogWrite(PINS::LED_TEST, lightLevel);
+	if (lightLevel < 255)
+	{
+		analogWrite(PINS::LED_TEST, lightLevel);
+	}
 
 	if (lightLevel == 255 || lightLevel == 0)
+	{
+		noInterrupts();
+		TIMSK1 &= (B11111110 << OCIE1A);
+		TCNT1 = 0;
+		
+		Serial.println("Job DONE");
+		interrupts();
+	}
+
+	/*
+	if (lightLevel == 255 || lightLevel == 0)
 		diff *= -1;
+
+		*/
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-
-}
-
-void CheckLight()
-{
-
-}
 
 
 void StartUp()
