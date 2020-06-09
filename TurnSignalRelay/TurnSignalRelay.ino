3952ddb2ee59dbce1734c9c8907a47cf1b1dae2b
Jirka Procházka <jirkaprochazka@post.cz>
2020-06-09 13:31:12 +0200

    New project TurnSignalRelay

---
+++ TurnSignalRelay/TurnSignalRelay.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TurnSignalRelay.ino
+ Created:	6/9/2020 1:29:33 PM
+ Author:	proch
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
